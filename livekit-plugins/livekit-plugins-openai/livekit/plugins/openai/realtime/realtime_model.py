707a23f444a51bf7fb9a86a66d45de93933c2c27
Rami Kalai <rami@compose.im>
2024-11-26 11:23:51 +0000

    test

--- livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/realtime/realtime_model.py
+++ livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/realtime/realtime_model.py
@@ -765,7 +765,7 @@ class RealtimeSession(utils.EventEmitter[EventTypes]):
         self._fnc_tasks = utils.aio.TaskSet()
         
         # Handle session expiration by notifying the model to renew
-        self.on("session_expired", lambda: self._loop.call_soon(lambda: self.renew_session()))
+        self.on("session_expired", lambda: self._loop.create_task(self.renew_session()))
         # Trigger session expiration for testing purposes
         asyncio.get_event_loop().call_later(65, lambda: self.emit("session_expired"))
     
